8304fdac696ea391aef57e60b3cc77fe86935e36
Asko Nõmm <asko@bien.ee>
2021-10-13 00:45:04 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,15 @@
   :description ""
   :url ""
   :license {}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.github.clj-easy/graal-build-time "0.1.4"]]
+  :plugins [[lein-shell "0.5.0"]
+            [lein-ancient "0.7.0"]]
+  :aliases
+  {"native"
+   ["shell"
+    "native-image" "--report-unsupported-elements-at-runtime" "--no-fallback"
+    "-jar" "./target/shh.jar"]}
   :main shh.core
   :aot [shh.core]
   :uberjar-name "shh.jar"
